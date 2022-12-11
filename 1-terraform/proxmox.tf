e0467e824a07dd2210938f8456356c9442e272ab
Amey Parulekar <amey@wirywolf.com>
2022-12-10 16:41:21 -0800

    Switch to using packer-created CoreOS template for test k3s node.

--- 1-terraform/proxmox.tf
+++ 1-terraform/proxmox.tf
@@ -1,7 +1,7 @@
 resource "proxmox_vm_qemu" "k3s-test-worker" {
   name = "k3s-test-worker"
   desc = "k3s test worker node"
-  target_node = var.proxmox.host
+  target_node = var.proxmox.node
   os_type = "cloud-init"
   full_clone = true
   memory = 2048
@@ -9,19 +9,19 @@ resource "proxmox_vm_qemu" "k3s-test-worker" {
   cores = 2
   cpu = "host"
   scsihw = "virtio-scsi-pci"
-  clone = "ubuntu-2204-cloudinit-template"
+  # clone = "ubuntu-2204-cloudinit-template"
+  clone = "k3s-coreos-2204"
   agent = 1
 
   network {
     model = "virtio"
     bridge = "vmbr0"
-    tag = 80
   }
 
   disk {
     type = "virtio"
     storage = "local-lvm"
-    size = "51200M"
+    size = "45G"
   }
 
   # Cloud-init section
