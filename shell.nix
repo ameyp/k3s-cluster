939ef0c8873cf3b53e30765661538af326cdd400
Amey Parulekar <amey@wirywolf.com>
2022-12-25 16:49:55 -0800

    Add tanka to shell.nix.

--- shell.nix
+++ shell.nix
@@ -3,12 +3,14 @@
 pkgs.mkShell {
   nativeBuildInputs = [
     pkgs.butane
+    # pkgs.fluxcd
     pkgs.jsonnet
     pkgs.jsonnet-bundler
     pkgs.jsonnet-language-server
     pkgs.kubernetes-helm
     pkgs.minikube
     pkgs.packer
+    pkgs.tanka
     pkgs.terraform
     pkgs.vault
   ];
